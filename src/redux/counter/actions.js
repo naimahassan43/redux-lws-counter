ce8c120f1e12bdee5e46437a433af2b059c42135
Naima Hassan <naima.hassan434@gmail.com>
2022-08-23 12:45:21 +0100

    creating store

--- src/redux/counter/actions.js
+++ src/redux/counter/actions.js
@@ -1,9 +1,16 @@
-import { INCREMENT } from "./actionTypes"
+import { DECREMENT, INCREMENT } from "./actionTypes"
 
 
 export const increment = (value)=>{
     return {
         type: INCREMENT,
+        payload: value
+    }
+}
+
+export const decrement = (value)=>{
+    return {
+        type: DECREMENT,
         payload: value
     }
 }
